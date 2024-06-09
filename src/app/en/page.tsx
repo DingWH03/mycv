b33229d27a569273d31cf125ea4cba3cb3a54b3e
DWH <2521248869@qq.com>
2024-06-10 02:24:17 +0800

    modify again

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -1,14 +1,17 @@
 import { Metadata } from "next";
 import { RESUME_DATA_en } from "@/data/resume-data_en";
-
 import Page from '@/app/page-contents';
 
 // 定义语言标识变量，默认为 'en'
 let language = 'en';
+
+// 组件的显示名称
+const enPage = () => <Page language={language} />;
+enPage.displayName = "MyCV_en";
 
 export const metadata: Metadata = {
   title: `${RESUME_DATA_en.name} | ${RESUME_DATA_en.about}`,
   description: RESUME_DATA_en.summary,
 };
 
-export default () => <Page language={language} />;
+export default enPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import { Metadata } from "next";
 import { RESUME_DATA } from "@/data/resume-data";
-
 import Page from '@/app/page-contents';
 
 // 定义语言标识变量，默认为 'ch'
-let language = 'ch';
+const language = 'ch';
 
+// 页面组件
+const chPage = () => <Page language={language} />;
+chPage.displayName = "chPage";
+
+// 元数据
 export const metadata: Metadata = {
   title: `${RESUME_DATA.name} | ${RESUME_DATA.about}`,
   description: RESUME_DATA.summary,
 };
 
-export default () => <Page language={language} />;
+export default chPage;
